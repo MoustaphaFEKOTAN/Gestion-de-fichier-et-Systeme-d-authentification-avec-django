a2606c8527c97cc0c800e465fe27128ac2140f0a
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-09-12 21:19:09 +0100

    Mise en place de l'autorisation sur certaines pages

--- fotoblog/fotoblog/settings.py
+++ fotoblog/fotoblog/settings.py
@@ -148,3 +148,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Ajouter cette ligne pour rediriger les utilisateurs non authentifiés vers la page de connexion
+LOGIN_URL = 'login'
+
