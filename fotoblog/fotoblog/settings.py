17f6684b64c505d02df2804bbc42eb2dbbb390d6
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-09-12 19:46:41 +0100

    Mise en place de AUTH_USER_MODEL pour Indiquer à Django d'utiliser le modèle User personnalisé

--- fotoblog/fotoblog/settings.py
+++ fotoblog/fotoblog/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+
+AUTH_USER_MODEL = 'authentication.User' # Indiquer à Django d'utiliser le modèle User personnalisé
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
